53125303fb9d51a5929d7709310b983b5eeb4a51
Oliver Steele <steele@osteele.com>
2017-12-02 11:55:14 -0500

    Add packages, some classifiers, to setup

--- setup.py
+++ setup.py
@@ -13,20 +13,29 @@ PKG_VERSION = re.search(
 setup(
     name='nbcollate',
     version=PKG_VERSION,
-    scripts=glob(os.path.join('scripts', '*')),
-    description='Collating Jupyter Notebooks',
-    long_description='Collate Jupyter assignment notebooks',
-    classifiers=[
-        'Intended Audience :: Developers', 'Intended Audience :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5'
-    ],
+    description='Collate Jupyter Notebooks',
+    long_description='Collate Jupyter classroom assignment and submission notebooks',
     url='http://github.com/olin-computing/nbcollate',
     author='Paul Ruvolo, Oliver Steele',
     author_email='steele@osteele.com',
     license='MIT',
+    packages=['nbcollate'],
+    scripts=glob(os.path.join('scripts', '*')),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        'Topic :: Education',
+
+    ],
     install_requires=[
         'cached_property',
         'nbconvert',
@@ -34,10 +43,4 @@ setup(
         'numpy',
         'python-Levenshtein',
     ],
-    setup_requires=[
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-    ]
 )
