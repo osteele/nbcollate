5c45f4262110cc4e701c35b0eb2097ca9127547b
Oliver Steele <steele@osteele.com>
2018-01-06 15:18:31 -0500

    Replace setuptools by flit

--- nbcollate/__init__.py
+++ nbcollate/__init__.py
@@ -1,2 +1,4 @@
+"Collate Jupyter classroom assignment and submission notebooks"
+
 from .nbcollate import nbcollate, remove_duplicate_answers, sort_answers, get_answer_tuples
 from .version import __version__
