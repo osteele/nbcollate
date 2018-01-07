fa9179a69b9a40cd9b146ee78826eb29381bbfb7
Oliver Steele <steele@osteele.com>
2018-01-07 17:23:02 -0500

    Export nb_clear_outputs

--- nbcollate/__init__.py
+++ nbcollate/__init__.py
@@ -2,4 +2,4 @@
 
 __version__ = "0.3.0"
 
-from .nbcollate import nbcollate, remove_duplicate_answers, sort_answers, get_answer_tuples
+from .nbcollate import nbcollate, nb_clear_outputs, remove_duplicate_answers, sort_answers, get_answer_tuples
