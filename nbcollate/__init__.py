faad051f139b9b94bc2e34d63548c992c3df3671
Oliver Steele <steele@osteele.com>
2018-01-07 18:03:56 -0500

    Bump version: 0.3.0 → 0.3.1

--- nbcollate/__init__.py
+++ nbcollate/__init__.py
@@ -1,5 +1,5 @@
 "Collate Jupyter classroom assignment and submission notebooks"
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .nbcollate import nbcollate, nb_clear_outputs, remove_duplicate_answers, sort_answers, get_answer_tuples
